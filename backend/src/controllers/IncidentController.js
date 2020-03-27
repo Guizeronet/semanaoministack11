34966215206cd42add746bfdedf71e3ab7ce9805
Guilherme Richard <gui401@live.com>
2020-03-27 13:47:22 -0300

    Testes no back-end

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -48,7 +48,7 @@ module.exports = {
         .select('ong_id')
         .first()
 
-        if(incident.ong_id != ong_id) {
+        if(incident == undefined || incident.ong_id != ong_id) {
             return response.status(401).json({error: 'Operatio not permitted.'})
         }
 
